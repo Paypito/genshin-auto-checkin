8195feb39f04fe7e3317e2d25377268994e882b6
e180039 <a.fernandez@student.helmo.be>
2021-04-04 18:59:39 +0200

    Add details logs dans intervalmod

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ let hour = config.hour;
 let minute = config.minute;
 
 (async () => {
-  console.log("Démarrage du bot ...");
+  console.log("[" + dayjs(Date.now()).format("HH:mm:ss") + "]", "Démarrage du bot ...");
   await getCookies();
   if (!isLogged()) login();
   else checkRewards();
@@ -22,7 +22,7 @@ async function login() {
   config.linux ? 
   browser = await puppeteer.launch({ headless: false, executablePath: config.chromium}) :
   browser = await puppeteer.launch({ headless: false });
-  console.log("Vous n'êtes pas connecté !");
+  console.log("[" + dayjs(Date.now()).format("HH:mm:ss") + "]", "Vous n'êtes pas connecté !");
   const page = await browser.newPage();
   await page.goto(
     config.url
@@ -33,11 +33,11 @@ async function login() {
       cookies = cook;
     })
     .catch(e => {
-      console.log("Une erreur est survenue, veuillez relancer")
+      console.log("[" + dayjs(Date.now()).format("HH:mm:ss") + "]", "Une erreur est survenue, veuillez relancer")
     });
     await new Promise(r => setTimeout(r, 5000));
   }
-  console.log("Connexion reussie !")
+  console.log("[" + dayjs(Date.now()).format("HH:mm:ss") + "]", "Connexion reussie !")
   await browser.close();
   saveCookies();
   checkRewards();
@@ -56,12 +56,18 @@ async function checkRewards() {
     config.url
   ).then(() => {
     getRewards(page);
-    schedule.scheduleJob({hour: hour, minute: minute}, async() => {
-      getRewards(page);
-    });
+    let delay = (hour * 60 * 60 * 1000) + (minute * 60 * 1000);
+    config.intervalMode ? 
+    setInterval(() => { 
+      console.log("[" + dayjs(Date.now()).format("HH:mm:ss") + "]", "Nouvelle vérification en cours ...");
+      getRewards(page);}, delay) :
+      schedule.scheduleJob({hour: hour, minute: minute}, async() => {
+        console.log("[" + dayjs(Date.now()).format("HH:mm:ss") + "]", "Nouvelle vérification en cours ...");
+        getRewards(page);
+      });
   }).catch(async () => {
     browser.close();
-    console.log("Aucune connexion internet, nouvel essai dans 20 secondes ...")
+    console.log("[" + dayjs(Date.now()).format("HH:mm:ss") + "]", "Aucune connexion internet, nouvel essai dans 20 secondes ...");
     await new Promise(r => setTimeout(r, 20000));
     checkRewards()
   });
@@ -76,29 +82,31 @@ async function getRewards(_page: puppeteer.Page) {
     let active = (await _page.$$('div.components-home-assets-__sign-content_---active---36unD3')).length;
     
     if(numberOfDay == 0) {
-      console.log("Une erreur est survenue lors du chargement de la page ! Vérifiez que l'url encodée dans config.json est correcte");
+      console.log("[" + dayjs(Date.now()).format("HH:mm:ss") + "]", "Une erreur est survenue lors du chargement de la page ! Vérifiez que l'url encodée dans config.json est correcte");
       getRewards(_page);
     } else {
-      console.log("Vous avez récupéré " + received + " sur " + numberOfDay + " récompenses");
+      console.log("[" + dayjs(Date.now()).format("HH:mm:ss") + "]", "Vous avez récupéré " + received + " sur " + numberOfDay + " récompenses");
       if(active != 0) {
-        console.log("Récompense disponible !");
-        console.log("Tentative de récupération de la récompense ...");
+        console.log("[" + dayjs(Date.now()).format("HH:mm:ss") + "]", "Récompense disponible !");
+        console.log("[" + dayjs(Date.now()).format("HH:mm:ss") + "]", "Tentative de récupération de la récompense ...");
         await _page.click('.components-home-assets-__sign-content_---active---36unD3');
         await new Promise(r => setTimeout(r, 2000));
         active = (await _page.$$('div.components-home-assets-__sign-content_---active---36unD3')).length;
         if(active == 0) {
-          console.log("La récompense à bien été récupérée !")
+          console.log("[" + dayjs(Date.now()).format("HH:mm:ss") + "]", "La récompense à bien été récupérée !")
         } else {
-          console.log("Une erreur empêche la récupération de la récompense")
+          console.log("[" + dayjs(Date.now()).format("HH:mm:ss") + "]", "Une erreur empêche la récupération de la récompense")
         }
       } else {
-        console.log("Aucune récompense disponible");
+        console.log("[" + dayjs(Date.now()).format("HH:mm:ss") + "]", "Aucune récompense disponible");
       }
-      console.log("Prochaine tentative à", dayjs().hour(hour).minute(minute).format('HH:mm'));
+      config.intervalMode ?
+      console.log("[" + dayjs(Date.now()).format("HH:mm:ss") + "]", "Prochaine tentative dans:", dayjs().hour(hour).minute(minute).format('HH:mm')):
+      console.log("[" + dayjs(Date.now()).format("HH:mm:ss") + "]", "Prochaine tentative à", dayjs().hour(hour).minute(minute).format('HH:mm'));
     }
   })
   .catch(async () => {
-    console.log("Aucune connexion internet, nouvel essai dans 20 secondes ...")
+    console.log("[" + dayjs(Date.now()).format("HH:mm:ss") + "]", "Aucune connexion internet, nouvel essai dans 20 secondes ...")
     await new Promise(r => setTimeout(r, 20000));
     getRewards(_page);
   });
@@ -110,14 +118,14 @@ function isLogged() {
 }
 
 async function saveCookies() {
-  console.log("Sauvegarde du cookie en cours ... ")
+  console.log("[" + dayjs(Date.now()).format("HH:mm:ss") + "]", "Sauvegarde du cookie en cours ... ")
   await fs.promises.writeFile('./cookies.json', JSON.stringify(cookies, null, 2));
 }
 
 async function getCookies() {
   await fs.promises.readFile('./cookies.json').
   then((cook) => {
-    console.log("Récupération du cookie de connexion ... ");
+    console.log("[" + dayjs(Date.now()).format("HH:mm:ss") + "]", "Récupération du cookie de connexion ... ");
     cookies = JSON.parse(cook.toString());
   })
   .catch(() => {
